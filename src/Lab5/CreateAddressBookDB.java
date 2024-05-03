ad1b976fcdb4582778c00d95544cf9a960f9f116
steven-halla <steven.r.halla@gmail.com>
2024-05-03 15:03:10 -0700

    creating DB creation class

---
+++ src/Lab5/CreateAddressBookDB.java
@@ -0,0 +1,30 @@
+
+package Lab5;
+
+
+
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Class        CreateAddressBookDB.java
+ * Description  A class representing the GUI used in the Address Book 
+ *              Application. This program that stores, retrieves, adds, and 
+ *              updates addresses using SQL statements in Address Book DB.
+ * Platform     jdk 1.8.0_241; NetBeans IDE 11.3; PC Windows 10
+ * Course       CS 143
+ * Hourse       4 hours and 17 minutes
+ * Date         4/5/2021
+  History Log   7/18/2018, 5/7/2020
+ * @author	<i>Niko Culevski</i>
+ * @version 	%1% %2%
+ * @see     	javax.swing.JFrame
+ * @see         java.awt.Toolkit 
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+public class CreateAddressBookDB {
+
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
